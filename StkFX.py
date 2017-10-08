66d1d5ec2d9f08a5466987d238c3cf91a5a60704
topsamxia <32593337+topsamxia@users.noreply.github.com>
2017-10-09 00:46:42 +0800

    First runnable traverse
    Traverse is ready. Multi-thread is ready. While the strategy is wrong. To be udpated.

--- StkFX.py
+++ StkFX.py
@@ -9,14 +9,7 @@ import pandas as pd
 import numpy as np
 import tushare as ts
 import os, time, datetime
-
-# Zhang Ting
-def zhangTing(close_old, close_new):
-    return float(close_new) / float(close_old) >= 1.098
-
-# Die Ting
-def dieTing(close_old, close_new):
-    return float(close_new) / float(close_old) <= 0.902
+import concurrent.futures
 
 # Shang Zhang
 def shangZhang(close_old, close_new, factor = 3):
@@ -26,9 +19,20 @@ def shangZhang(close_old, close_new, factor = 3):
 def xiaDie(close_old, close_new, factor = -3):
     return float(close_new) / float(close_old) <= factor/100 + 1
 
+# Zhang Ting
+def zhangTing(close_old, close_new):
+    return shangZhang(close_old, close_new, 9.8)
+
+# Die Ting
+def dieTing(close_old, close_new):
+    return xiaDie(close_old, close_new, -9.8)
+
+# structure that to hold single transaction or daily summary
 class SingleDeal(object):
     'single deal data struct'
-    def __init__(self, date="", time="", open = 0.0, close = 0.0, high = 0.0, low = 0.0, volume = 0.0, total = 0.0, zt = False, dt = False):
+    def __init__(self, code="", is_buy = True, date="", time="", open = 0.0, close = 0.0, high = 0.0, low = 0.0, volume = 0, total = 0.0, zt = False, dt = False):
+        self.code = code
+        self.is_buy = is_buy
         self.date = date
         self.time = time
         self.open = open
@@ -40,8 +44,25 @@ class SingleDeal(object):
         self.zt = zt
         self.dt = dt
 
+    def __str__(self):
+        return str([self.code, \
+                    self.is_buy, \
+                    self.date, \
+                    self.time, \
+                    self.open, \
+                    self.close, \
+                    self.high, \
+                    self.low, \
+                    self.volume, \
+                    self.total, \
+                    self.zt, \
+                    self.dt])
+
+    def __repr__(self):
+        return self.__str__()
+
     def getDeal(self):
-        return [self.date, self.time, self.open, self.close, self.high, self.low, self.volume, self.total]
+        return [self.code, self.is_buy, self.date, self.time, self.open, self.close, self.high, self.low, self.volume, self.total, self.zt, self.dt]
 
     def setDeal(self, dealInfo):
         if isinstance(dealInfo, pd.Series) :
@@ -55,7 +76,9 @@ class SingleDeal(object):
             self.total = dealInfo.total
         else:
             if isinstance(dealInfo, SingleDeal):
-                self.date = dealInfo.name
+                self.code = dealInfo.code
+                self.is_buy = dealInfo.is_buy
+                self.date = dealInfo.date
                 self.time = dealInfo.time
                 self.open = dealInfo.open
                 self.close = dealInfo.close
@@ -63,108 +86,170 @@ class SingleDeal(object):
                 self.low = dealInfo.low
                 self.volume = dealInfo.volume
                 self.total = dealInfo.total
+                self.zt = dealInfo.zt
+                self.dt = dealInfo.dt
+
 
 class SimulateDeal(object):
-    'transaction simulator'
+    'transaction simulator, obtain data, process data, store result'
     def __init__(self, balance = 0.0):
-        self.record = []
-        self.last_buy_time = ("1980/1/1", "00:00")
+
+        # basic properties
+        self.last_buy_time = ("1980/1/1", "00:00") # date, time
         self.last_sell_time = ("1980/1/1", "00:00")
+        self.last_transaction_time = ("1980/1/1", "00:00")
         self.balance = balance
-        self.hold = {}
-        self.occupied = False
-        self.daily_lock = {}
+
+        # transaction records and holdings
+
+        self.records = []    # series of SingleDeals
+        self.holding = {}      # map of code -> [quantity, average price]
+        self.occupied = False   # total balance is occupied
+        self.sell_lock = {}    # map of code->volume
+
+    # to update the hold value for all holdings
+    def updateValue(self):
+        pass
 
     # for string representative
     def __str__(self):
         return ("Balance =" + str(self.balance) + "\n" \
-                + "Hold = " + str(self.hold) + "\n" \
-                + "Record =" + str(self.record))
+                + "Holding = " + str(self.holding) + "\n" \
+                + "Records =" + str(self.records))
 
     # reset the daily lock
     def resetDaily(self):
-        self.daily_lock = {}
+        self.sell_lock = {}
 
     # evaluate daily worth
     def evalWorth(self, priceDict = {}):
         balance = self.balance
-        for code in self.hold.keys():
+        for code in self.holding.keys():
             if code in priceDict.keys():
-                balance += priceDict[code] * self.hold[code] * 100
+                balance += priceDict[code] * self.holding[code] * 100
         return balance
 
+    # update the buy action after the
+    def updateRestrictions(self, is_buy = True, date="", time="", code="", volume=0):
+        if is_buy:
+            if self.last_buy_time[0] != date:
+                self.sell_lock = {}
+            else:
+                if code in self.sell_lock.keys():
+                    self.sell_lock[code] += volume
+                else:
+                    self.sell_lock[code] = volume
+            self.last_buy_time = (date, time)
+            self.last_transaction_time = (date, time)
+        else: # is sell
+            self.last_sell_time = (date, time)
+            self.last_transaction_time = (date, time)
+
     #percentage various between 0~1, means the percentage of the balance for this buy
-    def buy(self, date="", time="", code = "", price=0.1, percentage=1):
-
+    def buy(self, date="", time="", code="", price=0.1, percentage=1.0):
         # calculate affordable volume
-        amount = int((self.balance*percentage/price)//100)
-
+        shou_amount = int((self.balance*percentage/price)//100)
         # not enough money, return
-        if amount <= 0:
+        if shou_amount <= 0:
             return False
         else:
             # update the record amounts
             # reduce balance
-            self.balance -= price*amount*100
+            expenditure = price*shou_amount*100
+            self.balance -= expenditure
             # set occupied = True if short of money for a share
             if self.balance/price//100 <= 0: self.occupied = True
 
-            self.record.append(("buy", date, time, code, price, amount))
+            dealInfo = SingleDeal(code = code, \
+                                  is_buy = True, \
+                                  date = date, \
+                                  time = time, \
+                                  open = price, close = price, high = price, low = price, \
+                                  volume = shou_amount, \
+                                  total = expenditure, \
+                                  zt = False, dt = False)
+            self.records.append(dealInfo)
 
-            if code in self.hold.keys():
-                self.hold[code] += amount
+            if code in self.holding.keys():
+                # original value = volume * price
+                original_value = self.holding[code][0]*100*self.holding[code][1]
+                # add the new buy expenditure
+                new_value = expenditure + original_value
+                # add the new volume
+                new_volume = self.holding[code][0]+shou_amount
+                # find the new average price of the holdings
+                new_price = new_value/new_volume/100.0
+                self.holding[code] = [new_volume, new_price]
             else:
-                self.hold[code] = amount
-            self.last_buy_time = (date, time)
+                self.holding[code] = [shou_amount, price]
 
-            if code in self.daily_lock.keys():
-                self.daily_lock[code] += amount
-            else:
-                self.daily_lock[code] = amount
+            # note down the sellable restrictions
+            self.updateRestrictions(True, date, time, code, shou_amount)
 
-            return True
+        return True
 
-    def getSellableAmount(self, date="", time="", code=""):
-        if not (code in self.hold.keys()): return 0
-
+    def getSellableVolume(self, date="", code=""):
+        # no stock to sell
+        if code not in self.holding.keys(): return 0
+        
         if date == self.last_buy_time[0]:
-            if not (code in self.daily_lock.keys()):
-                return self.hold[code]
+            if not (code in self.sell_lock.keys()):
+                return self.holding[code][0]
             else:
-                return self.hold[code]-self.daily_lock[code]
-        else: # assume the date difference means the new transaction happens later, so all holds are sellable
-            return self.hold[code]
+                return self.holding[code][0]-self.sell_lock[code]
+        else: # assume the date difference means the new transaction happens later, so all holdings are sellable
+            return self.holding[code][0]
 
 
-    def sell(self, date="", time="", code = "", price=0.1, amount=-1):
-        # return False if no such stk in hold, or zero number of hold, or not enough amount
-        if not (code in self.hold.keys()): return False
+    def sell(self, date="", time="", code = "", price=0.1, volume=-1):
+        # return False if no such stk in holding, or zero number of holding, or not enough amount
+        if not (code in self.holding.keys()): return False
 
-        # obtain amount that is sellable today
-        sellable_amount = self.getSellableAmount(date, time, code)
-        if sellable_amount == 0: return False
-
+        # obtain sellable volume today
+        sellable_volume = self.getSellableVolume(date, code)
+        if sellable_volume == 0: return False
         # default to sell all amount
-        if amount == -1: amount = sellable_amount
-        if self.hold[code] <= 0 \
-                or sellable_amount < amount: return False
+        if volume == -1: volume = sellable_volume
+
+        # Fail if not enough volume to sell
+        if sellable_volume < volume: return False
 
         # sell action
-        self.hold[code] -= amount
-        self.balance += amount*price*100
+        net_value_origin = self.holding[code][0]*self.holding[code][1]*100
+        net_sold = volume * 100 * price
+
+        self.balance += volume * 100 * price
         self.occupied = False
-        self.record.append(("sell", date, time, code, price, amount))
-        self.last_sell_time = (date, time)
+
+        self.holding[code][0] -= volume
+        if self.holding[code][0] == 0:
+            del self.holding[code]
+        else:
+            self.holding[code][1] = (net_value_origin - net_sold)/self.holding[code][0]/100
+
+        dealInfo = SingleDeal(code=code, \
+                              # not buy
+                              is_buy=False, \
+                              date=date, \
+                              time=time, \
+                              open=price, close=price, high=price, low=price, \
+                              volume=volume, \
+                              total=volume*100*price, \
+                              zt=False, dt=False)
+        self.records.append(dealInfo)
+
+        self.updateRestrictions(False, date, time, code, volume)
 
         return True
 
 class BaseSerialDeal(object):
-    'base class to hold stock data and common utilities'
+    'base class to holding stock data and common utilities'
 
     def __init__(self):
         self.type = 'base'
         self.initialized = False
 
+        self.code = ""
         self.data = None # store whole trade data
         self.date_index = None # store the date index as series
         self.date_list = [] # store date list as unique dates
@@ -175,12 +260,11 @@ class BaseSerialDeal(object):
         self.end_date = None
         self.last_update_time = datetime.datetime.now()
 
-        self.daily_summary = {}
+        self.daily_summary = {} # map of: date -> single_deal that to summary every date under current code
         self.dealer = SimulateDeal()
 
     def setBalance(self, balance):
         self.dealer.balance = balance
-
 
     def setDailySummary(self, single_deal):
         if not (single_deal is None):
@@ -193,7 +277,7 @@ class BaseSerialDeal(object):
         pass
 
 class TimeSerialDeal(BaseSerialDeal):
-    'base class to hold stock data and common utilities'
+    'base class to holding stock data and common utilities'
 
     def __init__(self):
         super(TimeSerialDeal, self).__init__()
@@ -214,7 +298,7 @@ class TimeSerialDeal(BaseSerialDeal):
         else:
             self.data = pd.read_csv(file_path)
 
-        self.data.set_index(["date", "time"], inplace=True)
+        self.data.set_index(index, inplace=True)
         [self.date_list.append(date_str) for date_str in self.data.index.get_level_values('date').unique()]
 
     def loadCsvNoHead(self, file_path='default.csv', index=["date", "time"]):
@@ -223,7 +307,7 @@ class TimeSerialDeal(BaseSerialDeal):
     def loadCsvHasHead(self, file_path='default.csv', index=["date", "time"]):
         return self.loadCsv(file_path, True, index)
 
-    def saveCsv(self, file_path='default.csv'):
+    def saveCsvWithHead(self, file_path='default.csv'):
         if isinstance(self.data, pd.DataFrame):
             self.data.to_csv(file_path)
 
@@ -233,11 +317,11 @@ class TimeSerialDeal(BaseSerialDeal):
             date_1 = self.date_list[abs_date_index - 1] # yesterday
             date_2 = self.date_list[abs_date_index - 2] # day before yesterday
             date_3 = self.date_list[abs_date_index - 3] # today -3
-            if (self.daily_summary[date_2].close * self.daily_summary[date_2].volume > 200000): # if volume is right
+            if (self.daily_summary[date_2].close * self.daily_summary[date_2].volume * 100 > 10000000): # if volume is right
                 # if day before yesterday is Zhang Ting,  and yesterday Shang Zhang, and today Xia Die
                 return (zhangTing(self.daily_summary[date_3].close, self.daily_summary[date_2].close) \
                         and shangZhang(self.daily_summary[date_2].close, self.daily_summary[date_1].close, 3.0) \
-                        and xiaDie(self.daily_summary[date_1], price, -3.0)) # if today xia Die -3% or more
+                        and xiaDie(self.daily_summary[date_1].close, price, -3.0)) # if today xia Die -3% or more
 
         return False
 
@@ -249,7 +333,7 @@ class TimeSerialDeal(BaseSerialDeal):
             date_3 = self.date_list[abs_date_index - 3] # today - 3
             date_4 = self.date_list[abs_date_index - 3] # today - 4
 
-            if (self.daily_summary[date_3].close * self.daily_summary[date_3].volume > 200000):
+            if (self.daily_summary[date_3].close * self.daily_summary[date_3].volume * 100 > 10000000):
                 # if day -3 is Zhangting, and Xia Die for 2 days, and now Xia Die again
                 return (zhangTing(self.daily_summary[date_4].close, self.daily_summary[date_3].close) \
                     and xiaDie(self.daily_summary[date_3].close, self.daily_summary[date_2].close) \
@@ -258,10 +342,20 @@ class TimeSerialDeal(BaseSerialDeal):
         return False
 
     def isQualifiedSellCondition1(self, date, time, code, price):
+        # over 5 days holding
         abs_date_index = self.date_list.index(date)
-        if abs_date_index - self.date_list.index(self.dealer.last_buy_time[0]) >= 5: return True
-        # to add more sell conditions, and add one record in dealer for current cost to calculate the gain
-        # if self.dealer.evalWorth({code, price}) / self.dealer.getCost(code) >= 1.08: return True
+        if (self.dealer.last_buy_time[0] in self.date_list) \
+            and (abs_date_index - self.date_list.index(self.dealer.last_buy_time[0]) >= 7):
+            return True
+
+        # over 8% profit
+        if code in self.dealer.holding.keys():
+            if price/self.dealer.holding[code][1] > 1.08: # more than 10% profit
+                return True
+
+        # to add more sell conditions, and add one records in dealer for current cost to calculate the gain
+
+        return False
 
     def dealActions(self, date, time, code, price):
         if self.isQualifiedCondition1(date, time, code, price) \
@@ -270,7 +364,7 @@ class TimeSerialDeal(BaseSerialDeal):
         if self.isQualifiedSellCondition1(date, time, code, price):
             self.dealer.sell(date, time, code, price)
 
-    def traverseDaily(self, date = "", determine_func = None, determine_param = ()):
+    def traverseDaily(self, date = "", code = "", determine_func = None, determine_param = ()):
 
         if self.data is None:
             pass
@@ -282,6 +376,9 @@ class TimeSerialDeal(BaseSerialDeal):
         daily_deal_status = SingleDeal()
         deal_open = daily_data.iloc[0]
         daily_deal_status.setDeal(deal_open) # set to first transaction numbers
+        daily_deal_status.code = code
+        daily_deal_status.volume = 0
+        daily_deal_status.total = 0
 
         for time in daily_data.index:
             current_deal = daily_data.loc[time]
@@ -297,10 +394,10 @@ class TimeSerialDeal(BaseSerialDeal):
             daily_deal_status.close = current_deal.close
 
             # trigger deal actions -------------------------------------------
-            self.dealActions(date, time, "300191", current_deal.close)
+            self.dealActions(date, time, code, current_deal.close)
             # ----------------------------------------------------------------
 
-        # store the daily deal status to the record
+        # store the daily deal status to the records
         self.daily_summary[date] = daily_deal_status
 
 
@@ -313,19 +410,38 @@ class TimeSerialDeal(BaseSerialDeal):
     def determineStrategy(self, determine_param = []):
         print (determine_param.index)
 
-
-    def traverseAll(self, determine_func = None, determine_param = ()):
+    def traverseAll(self, code="", determine_func=None, determine_param=()):
         for index, this_date in enumerate(self.date_list):
-            self.traverseDaily(this_date, determine_func, determine_param)
+            self.traverseDaily(this_date, code, determine_func, determine_param)
 
-
-
-if __name__ == '__main__':
+def dailyTest(filename=""):
     serial_data = TimeSerialDeal()
-    serial_data.loadCsv('hist_sample/SZ300191.csv')
+    serial_data.loadCsvHasHead(filename)
+    code = "000000" if len(filename) < 10 else filename[-10:-4]  # remove .csv
+
+    serial_data.dealer.balance = 100000
 
     for date in serial_data.date_list:
-        serial_data.traverseDaily(date)
+        serial_data.traverseDaily(date, code)
+
+    final_close = serial_data.daily_summary[serial_data.date_list[-1]].close
+    net_value = serial_data.dealer.evalWorth({code: final_close})
+    print(code + "----------------------------")
+    print(net_value)
+    print(serial_data.dealer)
+
+if __name__ == '__main__':
+
+    file_dir = os.path.abspath("D:\stock\YiQi_new")
+    files = os.listdir(file_dir)
+
+
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
+        for file in files:
+            file_name = os.path.join(file_dir, file)
+            executor.submit(dailyTest, file_name)
+
 
     # #print (serial_data)
     # serial_data.traverseDaily("2017/01/03", serial_data.determineStrategy, ("sdfe",))
